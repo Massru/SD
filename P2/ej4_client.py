690d8473c22caf8c9a01fe42b7381f846d2f258e
Massru <angelmassruiz@gmail.com>
2023-03-19 20:03:58 +0100

    Ej 4 FIN

--- P2/ej4_client.py
+++ P2/ej4_client.py
@@ -10,18 +10,27 @@ mensaje, addr = s.recvfrom(1024)
 
 nombre = input(mensaje.decode("utf-8"))
 
-s.sendto(bytes(nombre), direccion_servidor)
+s.sendto(nombre.encode("utf-8"), direccion_servidor)
 
-mensaje, addr = s.recvfrom(1024)
+ayuda, addr = s.recvfrom(1024)
 
-print(mensaje.decode("utf-8"))
+print(ayuda.decode("utf-8"))
 
 respuesta = input()
 
-s.sendto(bytes(respuesta), direccion_servidor)
+s.sendto(respuesta.encode("utf-8"), direccion_servidor)
 
-contestacion , addr = s.recvfrom(1024)
+while respuesta != 'exit':
 
-print(contestacion.decode("utf-8"))
+    contestacion , addr = s.recvfrom(1024)
+
+    print(contestacion.decode("utf-8"))
+
+    print(ayuda.decode("utf-8"))
+
+    respuesta = input()
+
+    s.sendto(respuesta.encode("utf-8"), direccion_servidor)
 
 s.close()
+print("Desconexión realizada")
--- P2/ej4_server.py
+++ P2/ej4_server.py
@@ -13,13 +13,20 @@ s.sendto("¡Bienvenido! ¿Cuál es su nombre para que pueda dirigirme a usted? "
 
 nombre, addr = s.recvfrom(1024)
 
-s.sendto(nombre + ", ¿en qué puedo ayudarte?".encode("utf-8"), addr)
+s.sendto(str(nombre.decode("utf-8")).encode("utf-8") + ", ¿en qué puedo ayudarte? ('exit' para salir)".encode("utf-8"), addr)
 
 respuesta, addr = s.recvfrom(1024)
 
 while respuesta.decode("utf-8") != 'exit':
 
-    if respuesta.decode("utf-8") == '¿Cúando empiezan las clases este curso?' :
+    if respuesta.decode("utf-8") == '¿Cuándo empiezan las clases este curso?' :
         s.sendto("Debe ponerse en contacto con el servicio de atención de dudas cuya dirección es dudas@ejemplo.com".encode("utf-8"), addr)
+    else:
+        s.sendto("Lo siento, no puedo ayudar".encode("utf-8"), addr)
+
+    #s.sendto(str(nombre.decode("utf-8")).encode("utf-8") + ", ¿en qué puedo ayudarte?".encode("utf-8"), addr)
+
+    respuesta, addr = s.recvfrom(1024)
 
 s.close()
+print("Desconexión realizada")
